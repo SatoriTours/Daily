9736af2ac065621c20262d66223081491faac66e
jimxl <tianxiaxl@gmail.com>
2025-03-11 22:40:24 +0800

    修复了备份恢复时候文件没有权限的问题

--- lib/app/modules/settings/controllers/settings_controller.dart
+++ lib/app/modules/settings/controllers/settings_controller.dart
@@ -1,4 +1,5 @@
 import 'package:daily_satori/app/services/web_service/web_service.dart';
+import 'package:daily_satori/global.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -6,6 +7,7 @@ import 'package:get/get.dart';
 import 'package:daily_satori/app/services/logger_service.dart';
 import 'package:daily_satori/app/repositories/setting_repository.dart';
 import 'package:daily_satori/app/services/setting_service/setting_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class SettingsController extends GetxController {
   final webServiceAddress = ''.obs;
@@ -28,6 +30,13 @@ class SettingsController extends GetxController {
 
   /// 选择备份目录
   void selectBackupDirectory() async {
+    // 检查是否有权限
+    final manageExternalStoragePermission = await Permission.manageExternalStorage.request();
+    if (!manageExternalStoragePermission.isGranted) {
+      errorNotice('请授予应用管理外部存储的权限');
+      return;
+    }
+
     final String? selectedDirectory = await FilePicker.platform.getDirectoryPath(
       dialogTitle: '选择备份文件夹',
       initialDirectory: SettingRepository.getSetting(SettingService.backupDirKey),
