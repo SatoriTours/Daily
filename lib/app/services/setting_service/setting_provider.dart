dc1cc4ebfcf48ab06d978d882cae2ee65232d7e6
jimxl <tianxiaxl@gmail.com>
2026-01-11 21:25:06 +0800

    重构了设置类

--- lib/app/services/setting_service/setting_provider.dart
+++ lib/app/services/setting_service/setting_provider.dart
@@ -2,56 +2,39 @@ import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:daily_satori/app/data/data.dart';
 
 class SettingProvider extends CacheProvider {
-  String _value(String key) {
-    return SettingRepository.i.getValue(key, defaultValue: '') ?? '';
-  }
+  String _value(String key) => SettingRepository.i.getValue(key, defaultValue: '') ?? '';
 
   @override
-  bool containsKey(String key) {
-    return SettingRepository.i.containsKey(key);
-  }
+  bool containsKey(String key) => SettingRepository.i.containsKey(key);
 
   @override
   bool? getBool(String key, {bool? defaultValue}) {
-    if (containsKey(key)) {
-      if (_value(key).toLowerCase() == 'true') {
-        return true;
-      }
-      return false;
-    }
-    return defaultValue;
+    if (!containsKey(key)) return defaultValue;
+    return _value(key).toLowerCase() == 'true';
   }
 
   @override
   double? getDouble(String key, {double? defaultValue}) {
+    if (!containsKey(key)) return defaultValue;
     try {
-      if (containsKey(key)) {
-        double.parse(_value(key));
-      }
-    } catch (e) {
-      // 解析失败时使用默认值
+      return double.parse(_value(key));
+    } catch (_) {
+      return defaultValue;
     }
-
-    return defaultValue;
   }
 
   @override
   int? getInt(String key, {int? defaultValue}) {
+    if (!containsKey(key)) return defaultValue;
     try {
-      if (containsKey(key)) {
-        int.parse(_value(key));
-      }
-    } catch (e) {
-      // 解析失败时使用默认值
+      return int.parse(_value(key));
+    } catch (_) {
+      return defaultValue;
     }
-
-    return defaultValue;
   }
 
   @override
-  Set getKeys() {
-    return SettingRepository.i.getKeys();
-  }
+  Set getKeys() => SettingRepository.i.getKeys();
 
   @override
   String? getString(String key, {String? defaultValue}) {
@@ -69,52 +52,38 @@ class SettingProvider extends CacheProvider {
     } else if (T == String) {
       return getString(key, defaultValue: defaultValue as String?) as T?;
     }
-    return defaultValue; // 如果 T 不是以上类型，返回默认值
+    return defaultValue;
   }
 
   @override
   Future<void> init() async {}
 
   @override
-  Future<void> remove(String key) async {
-    SettingRepository.i.removeByKey(key);
-  }
+  Future<void> remove(String key) async => SettingRepository.i.removeByKey(key);
 
   @override
-  Future<void> removeAll() async {
-    SettingRepository.i.removeAll();
-  }
+  Future<void> removeAll() async => SettingRepository.i.removeAll();
 
   @override
   Future<void> setBool(String key, bool? value) async {
-    if (value == true) {
-      SettingRepository.i.setValue(key, 'true');
-    } else {
-      SettingRepository.i.setValue(key, 'false');
-    }
+    SettingRepository.i.setValue(key, value == true ? 'true' : 'false');
   }
 
   @override
   Future<void> setDouble(String key, double? value) async {
-    if (value == null) {
-      SettingRepository.i.setValue(key, '0.0');
-    } else {
-      SettingRepository.i.setValue(key, value.toString());
-    }
+    SettingRepository.i.setValue(key, value?.toString() ?? '0.0');
   }
 
   @override
   Future<void> setInt(String key, int? value) async {
-    if (value == null) {
-      SettingRepository.i.setValue(key, '0');
-    } else {
-      SettingRepository.i.setValue(key, value.toString());
-    }
+    SettingRepository.i.setValue(key, value?.toString() ?? '0');
   }
 
   @override
   Future<void> setObject<T>(String key, T? value) async {
-    if (value is bool) {
+    if (value == null) {
+      await setString(key, null);
+    } else if (value is bool) {
       await setBool(key, value);
     } else if (value is double) {
       await setDouble(key, value);
@@ -123,16 +92,12 @@ class SettingProvider extends CacheProvider {
     } else if (value is String) {
       await setString(key, value);
     } else {
-      throw UnimplementedError('不支持的对象类型：${T.toString()}');
+      throw UnimplementedError('不支持的对象类型：$T');
     }
   }
 
   @override
   Future<void> setString(String key, String? value) async {
-    if (value == null) {
-      SettingRepository.i.setValue(key, '');
-    } else {
-      SettingRepository.i.setValue(key, value);
-    }
+    SettingRepository.i.setValue(key, value ?? '');
   }
 }
