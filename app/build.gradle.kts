3cc8f9583bf4fff2c2a4c16bb34a20845484497f
jimxl <tianxiaxl@gmail.com>
2026-05-21 09:22:54 +0800

    Release v5.0.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.dailysatori"
         minSdk = 26
         targetSdk = 36
-        versionCode = 50019
-        versionName = "5.0.19"
+        versionCode = 50020
+        versionName = "5.0.20"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
